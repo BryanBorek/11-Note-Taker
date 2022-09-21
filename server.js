ceeb0de8965ed0d04e460a14436626b6079c8fb0
Bryan Borek <bryantborek@gmail.com>
2022-09-21 15:41:08 -0500

    setting up a serverjs file to make the database work

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const fs = require('fs');
+const db = require('./db/db.json');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
