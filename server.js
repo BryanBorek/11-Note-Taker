e6671acf42a5a028bba03190f99fbf3fb9316d26
Bryan Borek <bryantborek@gmail.com>
2022-09-21 18:28:31 -0500

    pushing to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require('./db/db.json');
 const app = express();
 const path = require('path');
 
-const PORT = 'https://bryans-note-taker.herokuapp.com/';
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
