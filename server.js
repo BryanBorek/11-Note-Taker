1470034ab84aee1cdfb01b31d891a81908009674
Bryan Borek <bryantborek@gmail.com>
2022-09-21 17:03:31 -0500

    fixed errors in api routes in my serverjs file Got writing notes working

--- server.js
+++ server.js
@@ -9,3 +9,25 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+//Handling writing Notes
+app.get('/api/notes', (req, res) => res.json(db));
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = db.length;
+    const newNote = req.body;
+    db.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(db, null, 2));
+    res.json(newNote);
+}
+);
+
+//API routes
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
